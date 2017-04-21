ed7f5594da5a2f9c807e49d6c74c40fd35d50afc
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-04-21 11:14:05 -0300

    Removendo código duplicado

--- com/dms/example/EmployeeTest.java
+++ com/dms/example/EmployeeTest.java
@@ -4,23 +4,24 @@ import javax.swing.JOptionPane;
 
 public class EmployeeTest{
 
+	public static void displaySalaryYearly(Employee employee){	
+		String message = employee.getSalaryYearly();
+		JOptionPane.showMessageDialog(null, message);
+		System.out.println(message);
+	}
+
 	public static void main(String[] args){
 		Employee laudeci = new Employee("Laudeci", "Rodrigues", 1400.0);
 		Employee junior = new Employee("Diorgenes", "Filho", 980.0);
-		String message = laudeci.getSalaryYearly();
-		JOptionPane.showMessageDialog(null, message);
-		System.out.println(message);
-		message = junior.getSalaryYearly();
-		JOptionPane.showMessageDialog(null, message);
-		System.out.println(message);
+
+		displaySalaryYearly(laudeci);
+		displaySalaryYearly(junior);
+		
 		// aumento salárial de 10%
 		laudeci.setSalary(laudeci.getSalary() * 1.1);
 		junior.setSalary(junior.getSalary() * 1.1);
-		message = laudeci.getSalaryYearly();
-		JOptionPane.showMessageDialog(null, message);
-		System.out.println(message);
-		message = junior.getSalaryYearly();
-		JOptionPane.showMessageDialog(null, message);
-		System.out.println(message);
+		
+		displaySalaryYearly(laudeci);
+		displaySalaryYearly(junior);
 	}
 }
